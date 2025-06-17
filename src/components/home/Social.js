defe034dddf1d14d7cf84945b3fb24a312a1d6c6
JaiminP <jaiminp.0416@gmail.com>
2025-06-17 16:29:22 -0400

    initial commit

---
+++ src/components/home/Social.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="https://www.instgram.com/love_behind_lens_" className="home__social-icon" target='_blank' >
+            <i className="uil uil-instagram"></i>
+        </a>
+        
+        <a href="https://www.instgram.com/love_behind_lens_" className="home__social-icon" target='_blank' >
+            <i className="uil uil-dribbble"></i>
+        </a>
+        
+        <a href="https://www.instgram.com/love_behind_lens_" className="home__social-icon" target='_blank' >
+            <i className="uil uil-github-alt"></i>
+        </a>
+    </div>
+  )
+}
+
+export default Social
---
+++ src/components/scrollup/Scrollup.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import "./scrollup.css";
+
+const Scrollup = () => {
+
+    window.addEventListener("scroll",function() {
+        const scrollUp = this.document.querySelector(".scrollup");
+
+        if (this.scrollY >=560) scrollUp.classList.add("show-scroll");
+        else scrollUp.classList.remove("show-scroll");
+    })
+
+  return (
+
+    <a href="#" className="scrollup">
+        < i className='bx  bx-arrow-up-stroke scrollup__icon '></i>
+    </a>
+  )
+}
+
+export default Scrollup
+
+
+
+
+
+
+//COMPLEX WITH THE USE OF A BUTTON
+
+// import React, { useState, useEffect } from 'react';
+// import "./scrollup.css";
+
+// const Scrollup = () => {
+
+// const [visible, setVisible] = useState(false);
+
+//   const toggleVisibility = () => {
+//     if (window.pageYOffset > 100) {
+//       setVisible(true);
+//     } else {
+//       setVisible(false);
+//     }
+//   };
+
+//   const scrollToTop = () => {
+//     window.scrollTo({
+//       top: 0,
+//       behavior: 'smooth',
+//     });
+//   };
+
+//   useEffect(() => {
+//     window.addEventListener('scroll', toggleVisibility);
+//     return () => window.removeEventListener('scroll', toggleVisibility);
+//   }, []);
+
+
+//   return (
+
+//         visible && (
+//             <div className="scrollup__container container">
+//                 <button href="#contact" className="button button--flex scrollup__button" onClick={scrollToTop}>
+//                                 < i className='bx  bx-arrow-up-stroke scrollup__button-icon '></i> 
+//                 </button>
+//             </div>
+
+//     )
+
+//   )
+// }
+
+// export default Scrollup
